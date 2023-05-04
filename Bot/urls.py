cbc03dfa4b49d4d8ac67ad841f7f24a36760f803
Mohammad Hosein <Mohammad.h.b.7798@gmail.com>
2023-05-04 18:28:05 +0430

    feat (views.py): add pagination
    add pagination in search function in views.py

--- Bot/urls.py
+++ Bot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import bot
+from .views import search
 
 urlpatterns = [
-    path('', bot)
+    path('', search)
 ]
--- Bot/views.py
+++ Bot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import SearchForm
 from .bot import search
 from .models import Channel
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -16,7 +17,10 @@ def search(request):
         #     results = Channel.objects.filter(keyword=keyword).order_by('-subscriber')
         if len(results) == 0:
             results = search(keyword)
-        return render(request, 'Bot/result.html', {'results': results})
+        paginator = Paginator(results, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'Bot/result.html', {'results': results, 'page_obj': page_obj})
     else:
         form = SearchForm()
     return render(request, 'Bot/search.html', {'form': form})
