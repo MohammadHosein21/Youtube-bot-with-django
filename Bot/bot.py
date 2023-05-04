d80dd85828da0f1aa2d3b21f6db5af9e4513dd96
Mohammad Hosein <Mohammad.h.b.7798@gmail.com>
2023-05-04 11:28:27 +0430

    Change SQLite to Postgres

---
+++ Bot/bot.py
@@ -0,0 +1,70 @@
+from googleapiclient.discovery import build
+import urllib.parse
+
+# دریافت کلمه از کاربر
+search_term = input("Enter the search term: ")
+
+# ایجاد ارتباط با API یوتیوب
+api_key = "AIzaSyDeAmMZ6raZiZkWi_AgLVIIq1kg9Pb7rP8"
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# جستجوی کانال‌های مرتبط با کلمه
+
+query = urllib.parse.quote(search_term)
+request = youtube.search().list(
+    part="id,snippet",
+    maxResults=50,
+    type='channel',
+    q=query,
+    order='relevance'
+)
+response = request.execute()
+nextPage = response['nextPageToken']
+# print(response['nextPageToken']
+# نمایش عنوان، تعداد فالورها و لینک اشتراک هر کانال
+count = 0
+for item in response['items']:
+    channelId = item['id']['channelId']
+    channelTitle = item['snippet']['title']
+
+    request = youtube.channels().list(
+        part="statistics",
+        id=channelId
+    )
+    response = request.execute()
+
+    subscriberCount = response['items'][0]['statistics']['subscriberCount']
+    channelUrl = f"https://www.youtube.com/channel/{channelId}"
+
+    print(f"Channel Title: {channelTitle}\nSubscriber Count: {subscriberCount}\nChannel URL: {channelUrl}\n")
+    count += 1
+print(count)
+print("###########################################################")
+if nextPage == 'CDIQAA':
+    request = youtube.search().list(
+        part="id,snippet",
+        maxResults=50,
+        type='channel',
+        q=query,
+        pageToken='CDIQAA',
+        order='relevance'
+    )
+    response = request.execute()
+    # print(response['nextPageToken'])
+    for item in response['items']:
+        channelId = item['id']['channelId']
+        channelTitle = item['snippet']['title']
+
+        request = youtube.channels().list(
+            part="statistics",
+            id=channelId
+        )
+        response = request.execute()
+
+        subscriberCount = response['items'][0]['statistics']['subscriberCount']
+        channelUrl = f"https://www.youtube.com/channel/{channelId}"
+
+        print(f"Channel Title: {channelTitle}\nSubscriber Count: {subscriberCount}\nChannel URL: {channelUrl}\n")
+        count += 1
+
+print(count)
