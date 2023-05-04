454822b2751644b31f2217ef09818210b99e6e07
Mohammad Hosein <Mohammad.h.b.7798@gmail.com>
2023-05-04 23:03:17 +0430

    fix (views.py): fix search function
    fix some bugs in search function in searchView

--- Bot/views.py
+++ Bot/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 
-def search(request):
+def searchView(request):
     if request.method == 'POST':
         keyword = request.POST['keyword']
         results = Channel.objects.filter(keyword=keyword).order_by('-subscriber')
@@ -17,7 +17,7 @@ def search(request):
         #     results = Channel.objects.filter(keyword=keyword).order_by('-subscriber')
         if len(results) == 0:
             results = search(keyword)
-        paginator = Paginator(results, 10)
+        paginator = Paginator(results, 20)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'Bot/result.html', {'results': results, 'page_obj': page_obj})
